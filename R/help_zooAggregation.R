67f727ca65ada73a100aa92d40332c1aef2fb7f9
z7H4AabqdYVj <jobrenne@outlook.com>
2016-03-23 16:23:47 +0100

    na.rm

--- R/help_zooAggregation.R
+++ R/help_zooAggregation.R
@@ -1,7 +1,7 @@
-help_zooAggregation <- function(data, aggr_days=1/24)
+help_zooAggregation <- function(data, aggr_days=1/24, na.rm)
 {
   aggr_vec <- floor(as.numeric(time(data)) / aggr_days)
-  data <- aggregate(x=data, by=aggr_vec, FUN=mean, na.rm=F)
+  data <- aggregate(x=data, by=aggr_vec, FUN=mean, na.rm=na.rm)
   data <- zoo(x =  coredata(data), order.by = chron(time(data) * aggr_days))
   
   return(data)
