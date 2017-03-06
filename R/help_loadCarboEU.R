be1264b6698da926d444cead5986212727ecf42e
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2017-03-06 17:35:33 +0100

    reorder datetime, after loop now

--- R/help_loadCarboEU.R
+++ R/help_loadCarboEU.R
@@ -30,27 +30,26 @@ help_loadCarboEU <- function(datapath = "/Users/brennerj/tmp/eve_f2_data/data/pr
   {
     # read station data
     dat <- read.csv(file = i, na.strings = c("-9999.0000"))
-    # extract date
-    date <- paste(dat$Day,dat$Month,dat$Year,sep="-")
-    # extract time
-    time <- paste(dat$Hour,dat$Minute,"00",sep=":")
-    # converte date and time into chon object
-    date_time <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
-                              out.format = list(dates="d-m-y", times="h:m:s"))
 
     # for the first file create data and datetime dummy
     if (i == station_files[1]) {
-      dummy_date_time <- date_time
       dummy <- dat
     # append following data to dummies
     } else {
-      dummy_date_time <- c(dummy_date_time, date_time)
       dummy <- rbind(dummy, dat)
     }
-
   }
+
+  # extract date
+  date <- paste(dummy$Day,dummy$Month,dummy$Year,sep="-")
+  # extract time
+  time <- paste(dummy$Hour,dummy$Minute,"00",sep=":")
+  # converte date and time into chon object
+  date_time <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
+                            out.format = list(dates="d-m-y", times="h:m:s"))
+
   # create zoo object
-  dat_zoo <- zoo::zoo(dummy[,-c(1:5)], dummy_date_time)
+  dat_zoo <- zoo::zoo(dummy[,-c(1:5)], date_time)
   # return zoo object
   return(dat_zoo)
 }
