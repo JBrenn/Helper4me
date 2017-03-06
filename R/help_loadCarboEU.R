e334ff8867373b8429712021c51e0190973eb5d4
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2017-03-06 17:21:15 +0100

    dummy for date time, solved this bug, documentation

--- R/help_loadCarboEU.R
+++ R/help_loadCarboEU.R
@@ -20,27 +20,38 @@
 help_loadCarboEU <- function(datapath = "/Users/brennerj/tmp/eve_f2_data/data/processed/eddy_carbo_europe",
                              station = "ESES1")
 {
+  # get filenames from datapth dir
   files <- dir(path = datapath, full.names = T)
-  ESES1_files <- files[grepl(pattern = station, x = files)]
+  # extract station files
+  station_files <- files[grepl(pattern = station, x = files)]
 
-  for (i in ESES1_files)
+  # loop over true_files
+  for (i in station_files)
   {
+    # read station data
     dat <- read.csv(file = i, na.strings = c("-9999.0000"))
+    # extract date
     date <- paste(dat$Day,dat$Month,dat$Year,sep="-")
+    # extract time
     time <- paste(dat$Hour,dat$Minute,"00",sep=":")
-    date_time <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
+    # converte date and time into chon object
+    date_time_ <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
                               out.format = list(dates="d-m-y", times="h:m:s"))
 
-    if (i == ESES1_files[1]) {
+    # for the first file create data and datetime dummy
+    if (i == true_files[1]) {
+      dummy_date_time <- date_time
       dummy <- dat
+    # append following data to dummies
     } else {
+      dummy_date_time <- c(dummy_date_time, date_time)
       dummy <- rbind(dummy, dat)
     }
 
   }
-
-  dat_zoo <- zoo::zoo(dummy[,-c(1:5)], date_time)
-
+  # create zoo object
+  dat_zoo <- zoo::zoo(dummy[,-c(1:5)], dummy_date_time)
+  # return zoo object
   return(dat_zoo)
 }
 
