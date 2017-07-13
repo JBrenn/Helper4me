0dae1e72714eb660e0b0f4a30e1aab24949d91cc
Johannes Brenner brennerj <brennerj@ces251m.intranet.ufz.de>
2017-07-13 13:47:30 +0200

    introduce message and return(NULL) if no data available for station

--- R/help_loadCarboEU.R
+++ R/help_loadCarboEU.R
@@ -24,40 +24,45 @@ help_loadCarboEU <- function(datapath = "/Users/brennerj/tmp/eve_f2_data/data/pr
   files <- dir(path = datapath, full.names = T)
   # extract station files
   station_files <- files[grepl(pattern = station, x = files)]
-
-  print("reading ...")
-  # loop over station_files
-  for (i in station_files)
-  {
-    print(i)
-    # read station data
-    dat <- read.csv(file = i, na.strings = c("-9999.0000"))
-
-    # for the first file create data and datetime dummy
-    if (i == station_files[1]) {
-      dummy <- dat
-    # append following data to dummies
-    } else {
-      dummy <- rbind(dummy, dat)
-    }
-  }
-
-  # extract date
-  date <- paste(dummy$Day,dummy$Month,dummy$Year,sep="-")
-  # extract time
-  time <- paste(dummy$Hour,dummy$Minute,"00",sep=":")
-  # converte date and time into chon object
-  date_time <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
-                            out.format = list(dates="d-m-y", times="h:m:s"))
-
-  # create zoo object
-  dat_zoo <- zoo::zoo(dummy[,-c(1:5)], date_time)
   
-  # finished
-  print(paste("finished reading station", ST, sep=" "))
-              
-  # return zoo object
-  return(dat_zoo)
+  if (length(station_files)==0) {
+    print(paste("Data for station ", station," not available. Returning NULL."))
+    return(NULL)
+  } else {
+    print("reading ...")
+    # loop over station_files
+    for (i in station_files)
+    {
+      print(i)
+      # read station data
+      dat <- read.csv(file = i, na.strings = c("-9999.0000"))
+      
+      # for the first file create data and datetime dummy
+      if (i == station_files[1]) {
+        dummy <- dat
+        # append following data to dummies
+      } else {
+        dummy <- rbind(dummy, dat)
+      }
+    }
+    
+    # extract date
+    date <- paste(dummy$Day,dummy$Month,dummy$Year,sep="-")
+    # extract time
+    time <- paste(dummy$Hour,dummy$Minute,"00",sep=":")
+    # converte date and time into chon object
+    date_time <- chron::chron(dates. = date, times. = time, format = list(dates="d-m-y", times="h:m:s"),
+                              out.format = list(dates="d-m-y", times="h:m:s"))
+    
+    # create zoo object
+    dat_zoo <- zoo::zoo(dummy[,-c(1:5)], date_time)
+    
+    # finished
+    print(paste("finished reading station", station, sep=" "))
+    
+    # return zoo object
+    return(dat_zoo)
+  }
 }
 
 
